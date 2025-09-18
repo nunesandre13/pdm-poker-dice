41a4ec59a73333ca94627f5a1edec82100822945
GuilhermeA50467 <A50467@alunos.isel.pt>
2025-09-18 17:31:22 +0100

    lobbies view

--- app/src/main/java/pt/isel/pdm/lobby/LobbyCreationView.kt
+++ app/src/main/java/pt/isel/pdm/lobby/LobbyCreationView.kt
@@ -1,2 +1,49 @@
 package pt.isel.pdm.lobby
 
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun LobbyCreationView(onCreateLobby: (String) -> Unit) {
+    var lobbyName by remember { mutableStateOf("") } //simples estado
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(24.dp),
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = "Create Lobby",
+            style = MaterialTheme.typography.headlineMedium
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+
+        OutlinedTextField(
+            value = lobbyName,
+            onValueChange = { lobbyName = it },
+            label = { Text("Name do Lobbyyyyyyyyyyyy") },
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Spacer(modifier = Modifier.height(24.dp))
+        Button(
+            onClick = { onCreateLobby(lobbyName) },
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text("Create Lobby")
+        }
+    }
+}
+
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun LobbyCreationViewPreview() {
+    LobbyCreationView(onCreateLobby = {})
+}
--- app/src/main/java/pt/isel/pdm/lobby/LobiesView.kt
+++ app/src/main/java/pt/isel/pdm/lobby/LobiesView.kt
@@ -1,2 +1,62 @@
 package pt.isel.pdm.lobby
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import pt.isel.pdm.profile.ProfileScreen
+
+@Composable
+fun LobbyListScreen(lobbies: List<String>, onJoinClick: (String) -> Unit) {
+    Column {
+        lobbies.forEach { lobby ->
+            Spacer(modifier = Modifier.height(32.dp))
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 8.dp),
+                colors = CardDefaults.cardColors(containerColor = Color.Red),
+            ) {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Text(text = lobby, style = MaterialTheme.typography.bodyLarge)
+                    Button(onClick = { onJoinClick(lobby) }) {
+                        Text("Join")
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    LobbyListScreen(
+        lobbies = listOf("Lobby 1", "Lobby 2", "Lobby 3"),
+        onJoinClick = {}
+    )
+}
+
