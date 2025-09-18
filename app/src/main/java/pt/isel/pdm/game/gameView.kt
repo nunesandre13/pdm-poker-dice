f3dd7caf66f1de93d01f42533255fd69cb0429a3
GuilhermeA50467 <A50467@alunos.isel.pt>
2025-09-18 10:08:56 +0100

    view game

--- app/src/main/java/pt/isel/pdm/game/gameView.kt
+++ app/src/main/java/pt/isel/pdm/game/gameView.kt
@@ -1,2 +1,49 @@
 package pt.isel.pdm.game
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.chelasmulti_playerpokerdice.R
+
+
+@Composable
+fun GameView() {
+    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier.padding(innerPadding)
+        ) {
+            Image(
+                painter = painterResource(R.drawable.dadoa),
+                contentDescription = ""
+            )
+            Spacer(modifier = Modifier.height(32.dp))
+            Image(
+                painter = painterResource(R.drawable.dadok),
+                contentDescription = ""
+            )
+
+
+            Spacer(modifier = Modifier.height(32.dp))
+            Button({}) {
+                Text("Roll Dices")
+            }
+            Text("Turn:")
+        }
+
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun GreetingPreview() {
+    GameView()
+}
