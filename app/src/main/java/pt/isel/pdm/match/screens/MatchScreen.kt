346fc32063b77484d015ae7588c0af14329d9a2d
nunesandre13 <andre.oliv.nunes@gmail.com>
2025-12-23 02:11:28 +0000

    match with errors

--- app/src/main/java/pt/isel/pdm/match/screens/MatchScreen.kt
+++ app/src/main/java/pt/isel/pdm/match/screens/MatchScreen.kt
@@ -6,12 +6,22 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import pt.isel.pdm.domain.state.LobbyError
+import pt.isel.pdm.domain.state.MatchError
+import pt.isel.pdm.lobby.viewmodel.LobbyViewModel
 import pt.isel.pdm.match.innerComposable.RoundScreen
 import pt.isel.pdm.match.viewModels.MatchGlobalStateUi
 import pt.isel.pdm.match.viewModels.MatchViewModel
+import pt.isel.pdm.ui.errorPresentation.ErrorPopUp
 
 @Composable
-fun MatchScreen(
+fun MatchScreen(matchViewModel: MatchViewModel, onMatchEnded: () -> Unit) {
+    MatchScreenContent(matchViewModel, onMatchEnded)
+    MatchScreenError(matchViewModel)
+}
+
+@Composable
+fun MatchScreenContent(
     matchViewModel: MatchViewModel,
     onMatchEnded: () -> Unit
 ) {
@@ -38,4 +48,14 @@ fun MatchScreen(
         }
     }
 }
+
+@Composable
+fun MatchScreenError(viewModel: MatchViewModel) {
+    when (val stateError = viewModel.errorState.collectAsState().value) {
+        is MatchError -> ErrorPopUp(stateError){
+            viewModel.dismissError()
+        }
+    }
+}
+
 
